19db268c699962a13d8e1b841103a6418c1dd83f
LarryHillyer <LarryHillyer@hotmail.com>
2016-02-03 14:12:39 -0700

    Moved addIssue to Question Controller

--- app/components/myQuestionController.js
+++ app/components/myQuestionController.js
@@ -1,3 +1,30 @@
 app.controller('myQuestionController', function($scope){
     $scope.dummy = "dummy";
+    
+    $scope.addIssue = function () {
+
+    if ($scope.newIssue) {
+        $scope.QAservice.push($scope.newIssue);
+        $scope.newissue = '';
+        $scope.questionId++;
+    }
+}
+issue: {
+
+}
+
+// function to add userName
+$scope.userName = []
+
+$scope.addUser = function () {
+
+    if ($scope.newUsername) {
+        $scope.todos.push($scope.newuserName);
+        $scope.newuserName = '';
+        $scope.questionId++;
+    }
+}
+userName: {
+
+}
 })
--- app/components/myResponsesController.js
+++ app/components/myResponsesController.js
@@ -88,29 +88,4 @@ var _issue = [{
 
 // function to add issue this belongs in myQuestionsController
    
-$scope.addIssue = function () {
 
-    if ($scope.newIssue) {
-        $scope.QAservice.push($scope.newIssue);
-        $scope.newissue = '';
-        $scope.questionId++;
-    }
-}
-issue: {
-
-}
-
-// function to add userName
-$scope.userName = []
-
-$scope.addUser = function () {
-
-    if ($scope.newUsername) {
-        $scope.todos.push($scope.newuserName);
-        $scope.newuserName = '';
-        $scope.questionId++;
-    }
-}
-userName: {
-
-}

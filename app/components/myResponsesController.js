efdebb31b2de67301869ad066d64f2413c5ec80b
John Kent <jf.kent@yahoo.com>
2016-02-03 13:56:31 -0700

    adds raw data to issues object

---
+++ app/components/myResponsesController.js
@@ -0,0 +1,116 @@
+app.controller('myResponsesController', function ($scope, QAservice) {
+    this.test = "does this work?"
+    $scope.questionId = 0
+    $scope.issue = []
+});
+
+app.service('QAService', function (issue) {
+
+    issue = [
+        {
+            type: "Question",
+            UserName: "John",
+            date: Date.now(),
+            topic_a: "kind of Apples",
+            question: "What kind of Apples do you like?",
+            comment: "I hate meally red delicious apples and am looking for an alternative",
+            votes: "",
+            solution: "",
+            questionId: 1,
+        },
+        {
+            type: "Response",
+            UserName: "Sara",
+            date: Date.now(),
+            topic_a: "kind of Apples",
+            question: "What kind of Apples do you like?",
+            comment: "I prefer my Mac Book Pro",
+            votes: "",
+            solution: "",
+            questionId: 1,
+        },
+        {
+            type: "Response",
+            UserName: "Larry",
+            date: Date.now(),
+            topic_a: "kind of Apples",
+            question: "What kind of Apples do you like?",
+            comment: "I prefer the sourness of a grannysmith",
+            votes: "",
+            solution: "",
+            questionId: 1,
+        },
+        {
+            type: "Question",
+            UserName: "James",
+            date: Date.now(),
+            topic_a: "Found Dog",
+            question: "Did anyone lose a dog?",
+            comment: "A dog followed me home from the River glenn subddivision please identify the missing dog to claim it",
+            votes: "",
+            solution: "",
+            questionId: 2,
+        },
+        {
+            type: "Question",
+            UserName: "Chance",
+            date: Date.now(),
+            topic_a: "Found Dog",
+            question: "Did anyone lose a dog?",
+            comment: "I'm tired of walking him, you can keep it",
+            votes: "",
+            solution: "",
+            questionId: 2,
+        },
+
+    ];
+
+    this.getIssue = function () {
+        return issue;
+    }
+
+
+
+});
+// here is our basic object
+var _issue = [{
+    type: "text",
+    UserName: "text",
+    date: "date",
+    topic_a: [],
+    question: [],
+    comment: "text",
+    votes: "number",
+    solution: "text",
+    questionID: "number",
+
+}]
+
+// function to add issue this belongs in myQuestionsController
+   
+$scope.addIssue = function () {
+
+    if ($scope.newIssue) {
+        $scope.QAservice.push($scope.newIssue);
+        $scope.newissue = '';
+        $scope.questionId++;
+    }
+}
+issue: {
+
+}
+
+// function to add userName
+$scope.userName = []
+
+$scope.addUser = function () {
+
+    if ($scope.newUsername) {
+        $scope.todos.push($scope.newuserName);
+        $scope.newuserName = '';
+        $scope.questionId++;
+    }
+}
+userName: {
+
+}

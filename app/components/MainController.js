4b33552dea308899ff80c96d0e6882e28a242478
LarryHillyer <LarryHillyer@hotmail.com>
2016-02-04 13:53:03 -0700

    Created Response View

--- app/components/MainController.js
+++ app/components/MainController.js
@@ -2,7 +2,9 @@ app.controller('MainController', function($scope) {
     $scope.my_question = true;
     
     $scope.dummy = "dummy";
-    $scope.myIssues = [];
+    $scope.myIssues = ["nutrition","cost","flavor"];
+    $scope.responses = ["oatmeal", "beans and rice", "whey powder"];
+    $scope.comments = [];
     
     $scope.addIssue = function () {
         $scope.myIssues.push($scope.newIssues);
@@ -10,15 +12,49 @@ app.controller('MainController', function($scope) {
  
     }
     
+    $scope.addComment = function () {
+        $scope.comments.push($scope.newComment);
+        $scope.newComment = "";   
+ 
+    }   
+    
+    $scope.goBack = function() {
+        $scope.activeQuestion = "";
+        $scope.activeResponse = "";
+    }
+    
     $scope.setActiveQuestion = function(issue){
         $scope.activeQuestion = issue;
     }
     
-    $scope.setView = function(view) {
-        if (view === 'myQuestion') {
-            this.my_question = true;
-        } else {
-            this.my_question = false;
-        }
+    $scope.setActiveResponse = function(response){
+        $scope.activeResponse = response;
+    }  
+    $scope.setActiveComment = function(comment){
+        $scope.activeComment = comment;
+    }    
+    
+    // $scope.setView = function(view) {
+    //     if (view === 'myQuestion') {
+    //         this.my_question = true;
+    //     } else {
+    //         this.my_question = false;
+    //     }
+    // }
+    
+    $scope.addResponse = function() {
+ 
+        $scope.responses.push($scope.newResponse);
+        $scope.newResponse = "";       
     }
 });
+
+// comment = {
+//     UserName: "John",
+//     date: Date.now(),
+//     body: "comment body this is the text",
+//     votes: "",
+//     questionId: 324,
+//     responseId: 2,
+//     id: 1
+// }
